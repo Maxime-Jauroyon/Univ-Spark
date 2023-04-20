8917bc7e54fd016951c969c385b4aaa4a1ddc4db
JAUROYON maxime <maxime.jauroyon@gmail.com>
2023-04-20 15:20:49 +0200

    Processing API data stream done

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ Downloading the Reddit API : pip install praw
 
 Downloading the NLP API : pip install textblob
 
-Link where we created our account we used : https://www.reddit.com/prefs/apps
+Downloading pyspark : pip install pyspark
+
+Link where we created our account we used : https://www.reddit.com/prefs/apps 
+(which currently don't work so the old link https://old.reddit.com/prefs/apps/ is what we really used)
+
+Spark Streaming Programming Guide : https://spark.apache.org/docs/latest/streaming-programming-guide.html
+
+How to run (local):
+first launch the server who will send the Reddit data : python3 server.py localhost 8888
+then you can run the notebook
+
+How to run (docker):
+first launch the server who will send the Reddit data : spark-submit server.py localhost 8888
+then you can run the notebook
--- server.py
+++ server.py
@@ -1,17 +1,22 @@
 import praw
 import socket
 import json
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: server.py <hostname> <port>", file=sys.stderr)
+    sys.exit(-1)
 
 # Define Reddit API credentials
-client_id = "zqRTjPqgLq6yqzJPBcYM8w" # YOUR_CLIENT_ID
-client_secret = "wLc64W4xQqtu05OuHyVDkhiWKIZGHQ" # YOUR_CLIENT_SECRET
-# username = "YOUR_USERNAME" not necessary for read-oly access
-# password = "YOUR_PASSWORD"
-user_agent = "web:mySparkProject:v1.0.0"# YOUR_USER_AGENT
+client_id = "zqRTjPqgLq6yqzJPBcYM8w" # OUR_CLIENT_ID
+client_secret = "wLc64W4xQqtu05OuHyVDkhiWKIZGHQ" # OUR_CLIENT_SECRET
+# username = "OUR_USERNAME" not necessary for read-only access
+# password = "OUR_PASSWORD"
+user_agent = "web:RedditStreamingSentimentAnalysis:v1.0.0"# OUR_USER_AGENT
 
 # Define socket host and port
-host = "localhost"
-port = 12345
+host = sys.argv[1] # "localhost"
+port = int(sys.argv[2]) # 8888
 
 # Create a Reddit instance and authenticate
 reddit = praw.Reddit(
@@ -36,18 +41,29 @@ print("Waiting for a client to connect...")
 client_socket, client_address = server_socket.accept()
 print(f"Client connected from {client_address}")
 
-# Iterate over the "hot" posts in the "python" subreddit
-for post in reddit.subreddit("python").hot(limit=10):
-    # Convert the post data to a JSON string
-    post_data = json.dumps({
-        "title": post.title,
-        "url": post.url,
-        "author": post.author.name,
-        "created_utc": post.created_utc,
-    })
-    # Send the post data to the client socket
-    client_socket.sendall(post_data.encode("utf-8"))
-    print(f"Sent data to client: {post_data}")
+
+subreddit = reddit.subreddit("python")
+for submission in subreddit.stream.submissions():
+    # message = ""
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list():
+        message = comment.body + " "
+        client_socket.send(message.encode('utf-8'))
+        # print(f"Sent data to client: {message}")
+ 
+
+# # Iterate over the "hot" posts in the "python" subreddit
+# for post in reddit.subreddit("python").hot(limit=10):
+#     # Convert the post data to a JSON string
+#     post_data = json.dumps({
+#         "title": post.title,
+#         "url": post.url,
+#         "author": post.author.name,
+#         "created_utc": post.created_utc,
+#     })
+#     # Send the post data to the client socket
+#     client_socket.sendall(post_data.encode("utf-8"))
+#     print(f"Sent data to client: {post_data}")
 
 # Close the client socket and server socket
 client_socket.close()
